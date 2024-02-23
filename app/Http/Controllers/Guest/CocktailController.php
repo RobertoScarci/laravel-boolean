7fba1cbd74cdef390c3bf3c4b1a3e76598259811
UmbertoSerino <serumberto@gmail.com>
2024-02-23 16:53:30 +0100

    Cocktail Controller modified

--- app/Http/Controllers/Guest/CocktailController.php
+++ app/Http/Controllers/Guest/CocktailController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Cocktail extends Controller
+class CocktailController extends Controller
 {
     //
 }
